40f1b1f4212f7e3225b21231a49992d1a3b1051f
Oran <oran@192.168.124.3>
2021-06-17 23:11:03 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="map"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted } from 'vue';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  // components: {
+  //   HelloWorld,
+  // },
+  setup() {
+    onMounted(() => {
+      console.log('bbb');
+    
+      new Map({
+        target: 'map',
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+          })
+        ],
+        view: new View({
+          center: [0, 0],
+          zoom: 2
+        })
+      });
+    })
   },
 });
 </script>
+
+<style lang="scss" scoped>
+#map{
+  height: 400px;
+}
+</style>
